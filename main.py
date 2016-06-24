4b87513f29f8f5cf8b13fbaa07157c85ea6f6dd0
Lily Ng <thn248@nyu.edu>
2016-06-24 13:19:19 -0400

    update

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from kivy.app import App
 
-from kivy.uix.pagelayout import PageLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.scatter import Scatter
 from kivy.uix.label import Label
-
-from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 
 
 class Alertify(App):
     def build(self):
-        main_layout = PageLayout()
-        logo = Label(text = "Alertify", font_size = 20)
-        main_layout.add_widget(logo)
-        '''layout = BoxLayout(orientation='vertical')
-        btn1 = Button(text='Hello')
-        btn2 = Button(text='World')
-        layout.add_widget(btn1)
-        layout.add_widget(btn2)'''
-        return main_layout
+        App_header = BoxLayout(orientation = "vertical")
+        logo = Label(text = "Alertify", font_size = 80)
+        add_button = Button(text = "Add")
+        alert_button = Button(text = "Alert")
+        App_header.add_widget(logo)
+        App_header.add_widget(add_button)
+        App_header.add_widget(alert_button)
+        return App_header
 
 Alertify().run()
