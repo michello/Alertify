e25f6dd1a8717d8ed8962c71959dadc57c06c565
ml4963 <ml4963@nyu.edu>
2016-06-24 13:10:08 -0400

    adding nothing really

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from kivy.uix.pagelayout import PageLayout
 from kivy.uix.scatter import Scatter
 from kivy.uix.label import Label
 
+from kivy.uix.widget import Widget
+
 
 class Alertify(App):
     def build(self):
         main_layout = PageLayout()
         logo = Label(text = "Alertify", font_size = 20)
         main_layout.add_widget(logo)
+        '''layout = BoxLayout(orientation='vertical')
+        btn1 = Button(text='Hello')
+        btn2 = Button(text='World')
+        layout.add_widget(btn1)
+        layout.add_widget(btn2)'''
         return main_layout
 
 Alertify().run()
--- twilio program.py
+++ twilio program.py
@@ -7,5 +7,5 @@ client = TwilioRestClient("ACbb4b9650631364e99781ded800fa9699", \
                           "0a068601c149167e134887defd6fc2a9")
 #account_sid, auth_tokens
 
-client.messages.create(to="+19176671977", from_="+16464193165",
-                                     body="Hello there!")
+client.messages.create(to="+19292498796", from_="+16464193165",
+                                     body="Hello Jia~!")
