0125ce73dd04486f62e6ca03570437e8879c3bb3
Lily Ng <thn248@nyu.edu>
2016-06-25 14:33:23 -0400

    update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from twilio.rest import TwilioRestClient
 from kivy.app import App
+from kivy.lang import Builder
 
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
@@ -8,45 +10,75 @@ from kivy.uix.scatter import Scatter
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 
+Builder.load_string("""
+<HomeScreen>:
+	BoxLayout:
+		orientation: "vertical"
+		Label:
+			text: "Alert!fy"
+			font_size: 80
+		Button:
+			text: "View Contact"
+			height: 80
+			on_press: root.manager.current = 'contacts'
+		Button:
+			text: "Alert"
+			on_press: root.send_Alert()
+		Button:
+			text: "Setting"
+			on_press: root.manager.current = 'settings'
+	
+<ContactScreen>:
+	BoxLayout:
+		orientation: "vertical"
+		Button:
+		Button:
+<SettingScreen>:
+	BoxLayout:
+		orientation: "vertical"
+		Button:
+		Button:
+""")
+
+# Set up the screens
+class HomeScreen(Screen):
+    '''
+    (From the twilio program)
+    function to send out alerts to the destination
+    '''
+    def send_Alert(self, *args):
+        account_sid = "ACbb4b9650631364e99781ded800fa9699" 
+        auth_token = "0a068601c149167e134887defd6fc2a9" 
+        client = TwilioRestClient("ACbb4b9650631364e99781ded800fa9699", \
+                              "0a068601c149167e134887defd6fc2a9")
+        client.messages.create(to="+19176671977", from_="+16464193165",
+                                         body="Hello Jia~!")
+        return
+
+class ContactScreen(Screen):
+    pass
+
+class SettingScreen(Screen):
+    pass
+
+# Add the screens
+sm = ScreenManager()
+sm.add_widget(HomeScreen(name = "home"))
+sm.add_widget(ContactScreen(name = "contacts"))
+sm.add_widget(SettingScreen(name = "setting"))
+
+#The functions to switch screen to "contacts"
 def view_Contacts(obj):
     #App proceeds to next page
     pass
 
-'''
-(From the twilio program)
-function to send out alerts to the destination
-'''
-def send_Alert(obj):
-    account_sid = "ACbb4b9650631364e99781ded800fa9699" 
-    auth_token = "0a068601c149167e134887defd6fc2a9" 
-    client = TwilioRestClient("ACbb4b9650631364e99781ded800fa9699", \
-                          "0a068601c149167e134887defd6fc2a9")
-    client.messages.create(to="+19176671977", from_="+16464193165",
-                                     body="Hello Jia~!")
-    return
-
+# Function to switch screen to "functions"
 def to_setting(obj):
     pass
 
+
 class AlertifyApp(App):
     def build(self):
-        App_header = BoxLayout(orientation = "vertical")
-        logo = Label(text = "Alertify", font_size = 80)
-        
-        add_button = Button(text = "Add")
-        add_button.bind(on_press = view_Contacts)
-        
-        alert_button = Button(text = "Alert")
-        alert_button.bind(on_press = send_Alert)
-        
-        
-        setting_button = Button(text = "Setting")
-        alert_button.bind(on_press = to_setting)
-        
-        App_header.add_widget(logo)
-        App_header.add_widget(add_button)
-        App_header.add_widget(alert_button)
-        App_header.add_widget(setting_button)
-        return App_header
+        return sm
 
 AlertifyApp().run()
